fe3cf96a141d5f6bee8faff1ab9793ddd021254e
hiskgp <srikeerthigp@gmail.com>
2026-07-03 03:12:43 +0530

    change default theme and remove opensource items

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,10 +20,10 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: {
-    default: "wacrm",
-    template: "%s — wacrm",
+    default: "redant",
+    template: "%s — redant",
   },
-  description: "Self-hostable CRM template for WhatsApp.",
+  description: "redant automation platform.",
   robots: {
     index: false,
     follow: false,
